99ea30cb673a8e795411457297423f8b867b2b18
Liam Fruzyna <liamfruzyna@gmail.com>
2019-12-30 17:09:41 -0600

    Switched to ZFS for volumes

--- backup.sh
+++ backup.sh
@@ -10,16 +10,14 @@ mkdir $BACKUP_DIR
 # enter maintenance mode
 docker exec --user www-data nextcloud-app php occ maintenance:mode --on
 # backup nextcloud folder
-rsync -Aax /var/lib/docker/volumes/docker-config_nextcloud-app/_data/ "$BACKUP_DIR/nextcloud/"
+rsync -Aax /lakefront/nextcloud/app "$BACKUP_DIR/nextcloud/"
 # backup mysql database
 docker exec nextcloud-db mysqldump --single-transaction -u nextcloud -p[password] nextcloud-db > "$BACKUP_DIR/nextcloud-db.bak"
 # leave maintenance mode
 docker exec --user www-data nextcloud-app php occ maintenance:mode --off
 
 # backup other containers
-cp -R /var/lib/docker/volumes/docker-config_reverse-proxy/_data/nginx/ "$BACKUP_DIR/nginx"
-cp -R /var/lib/docker/volumes/docker-config_home-assistant/_data/ "$BACKUP_DIR/home-assistant"
-cp -R /var/lib/docker/volumes/docker-config_plex/_data/ "$BACKUP_DIR/plex"
+cp -R /lakefront/config/home-assistant "$BACKUP_DIR/home-assistant"
 
 # optionally compress the directory
 #tar -zcf "$BACKUP_DIR.tar.gz" $BACKUP_DIR
